b982f252b95d8b6e2504f0e8fce43f1b2120773b
Khaled Mustafa <km282596@gmail.com>
2021-01-07 20:08:01 +0200

    add api to the project

--- pizzahouse/routes/api.php
+++ pizzahouse/routes/api.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+// CRUD system
+Route::apiResource('/pizza' , \App\Http\Controllers\PizzaApiController::class);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
